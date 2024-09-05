2b4c51e898a23650649036b933db5cf0b9ec1d1c
lovecoal0928 <lovecoal928@gmail.com>
2024-09-05 15:27:03 +0900

    feat: 簡単なルーティング設定ファイルを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { TopTask } from '@/constants/routing';
+import Link from 'next/link';
+
 export default function Home() {
 	return (
 		<main>
 			<h1>Home</h1>
+
+			<Link href={TopTask} className="text-blue-500">
+				今やるタスク画面へ
+			</Link>
 		</main>
 	);
 }
