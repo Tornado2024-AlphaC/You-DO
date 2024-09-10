70868cf7219aee59ec3428af8681a223f2b1279f
KHashimoto3 <hashi.koki385@gmail.com>
2024-09-10 13:46:36 +0900

    feat: タスク一覧を返すapiを作成した。

---
+++ src/app/api/task/[user_id]/list/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import spabase from "@/libs/spabase";
+
+export async function GET(_:any, { params }: { params: { user_id: string } }) {
+    if (!params.user_id) {
+        return NextResponse.json({
+            status: 400,
+            message: "Bad Request",
+        },{ status: 400 });
+    }
+    const user_id = Number(params.user_id);
+    if (isNaN(user_id)) {
+        return NextResponse.json({
+            status: 400,
+            message: "Bad Request",
+        },{ status: 400 });
+    }
+    try {
+        const { data, error } = await spabase.from("sample-Task").select("*").eq("user_id", user_id);
+        if (error) {
+            throw error;
+        }
+        if (data.length === 0) {
+            return NextResponse.json({
+                status: 404,
+                message: "Data not found",
+            },{ status: 404 });
+        }
+        return NextResponse.json({
+        taskList: data,
+        });
+    } catch (error) {
+        return NextResponse.json({
+            status: 500,
+            message: "Internal Server Error",
+        },{status: 500});
+    }
+}
