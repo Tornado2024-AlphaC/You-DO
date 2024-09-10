7cae38b658914376b76d0071b07d5447541f6ab4
KHashimoto3 <hashi.koki385@gmail.com>
2024-09-10 14:43:45 +0900

    improve: タスク一覧取得のapiについて、型情報を追記した。

--- src/app/api/task/[user_id]/list/route.ts
+++ src/app/api/task/[user_id]/list/route.ts
@@ -1,6 +1,24 @@
 import { NextResponse } from "next/server";
 import spabase from "@/libs/spabase";
 
+type Task = {
+    id: number;
+    user_id: number;
+    title: string;
+    limit_time: string;
+    parent_id: number;
+    available_break: boolean;
+    duration: number;
+    expectation: number;
+    urgency: number;
+    firstexpect: number;
+    progress: number;
+    priority: number;
+    skip_count: number;
+    created_at: string;
+    updated_at: string;
+}
+
 export async function GET(_:any, { params }: { params: { user_id: string } }) {
     if (!params.user_id) {
         return NextResponse.json({
@@ -20,14 +38,15 @@ export async function GET(_:any, { params }: { params: { user_id: string } }) {
         if (error) {
             throw error;
         }
-        if (data.length === 0) {
+        const taskList: Task[] = data;
+        if (taskList.length === 0) {
             return NextResponse.json({
                 status: 404,
                 message: "Data not found",
             },{ status: 404 });
         }
         return NextResponse.json({
-        taskList: data,
+        taskList,
         });
     } catch (error) {
         return NextResponse.json({
