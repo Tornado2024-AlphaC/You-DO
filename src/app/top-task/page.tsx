9b457fce27d67738359da975c861fdc8a36befa0
KHashimoto3 <hashi.koki385@gmail.com>
2024-09-15 16:37:57 +0900

    feat: 今やるべきタスク一覧取得のapiと接続した。

--- src/app/top-task/page.tsx
+++ src/app/top-task/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Footer from '@/components/features/footer/Footer';
-import React from 'react';
+import React, { useEffect } from 'react';
 import TaskCard from './components/taskCard/TaskCard';
 import { ResetBtn, ResetBtnDisabled } from './components/timer/ResetBtn';
 import { InTimer, NextTimer } from './components/timer/Timer';
@@ -10,8 +10,34 @@ import { useSwipeable } from 'react-swipeable';
 import { useRouter } from 'next/navigation';
 import { SideSwipe } from '@/components/ui/sideSwipe';
 
+import { useUserData } from '@/components/features/use-cookies/useUserData';
+
+type Task = {
+	id: number;
+	user_id: number;
+	title: string;
+	limit_time: string;
+	parent_id: number;
+	available_break: boolean;
+	duration: number;
+	expectation: number;
+	urgency: number;
+	firstexpect: number;
+	progress: number;
+	priority: number;
+	skip_count: number;
+	created_at: string;
+	updated_at: string;
+};
+
 const TopTask = () => {
 	const router = useRouter();
+
+	const { getUserData } = useUserData();
+	const user_id = getUserData().user_id;
+
+	const [taskList, setTaskList] = React.useState<Task[]>([]);
+	const [topTask, setTopTask] = React.useState<Task | null>(null);
 
 	const handlers = useSwipeable({
 		onSwipedLeft: () => router.push(TaskList),
@@ -20,6 +46,62 @@ const TopTask = () => {
 		trackMouse: true,
 	});
 
+	//API: 今やるべきタスクの一覧を取得する
+	const get_next_task_list = async (user_id: string): Promise<Task[]> => {
+		return new Promise((resolve, reject) => {
+			const url = `api/task/${user_id}/next`;
+			try {
+				fetch(url, {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}).then(async res => {
+					const contentType = res.headers.get('content-type');
+					if (!res.ok) {
+						const statusCode = res.status;
+						if (!contentType || !contentType.includes('application/json')) {
+							throw new Error("Oops, we haven't got JSON!");
+						}
+						switch (statusCode) {
+							case 400:
+								throw new Error('Bad Request');
+							case 500:
+								throw new Error('Internal Server Error');
+							default:
+								throw new Error('Unknown Error');
+						}
+					}
+					const data = await res.json();
+					resolve(data.nextTask);
+				});
+			} catch (error) {
+				alert('A:タスク一覧取得中にエラーが発生しました。');
+				reject(error);
+			}
+		});
+	};
+
+	useEffect(() => {
+		const call_get_next_task_list = async () => {
+			try {
+				const taskList = await get_next_task_list(user_id);
+				if (!taskList) {
+					throw new Error('TaskList is empty');
+				}
+				if (taskList.length === 0) {
+					alert('タスクがありません。');
+				}
+				setTaskList(taskList);
+				setTopTask(taskList[0]);
+			} catch (error) {
+				alert('B: タスク一覧取得中にエラーが発生しました。');
+				return;
+			}
+		};
+
+		call_get_next_task_list();
+	}, []);
 	return (
 		<SideSwipe>
 			<main {...handlers}>
@@ -28,7 +110,18 @@ const TopTask = () => {
 					<ResetBtnDisabled />
 				</div>
 
-				<TaskCard />
+				{topTask && (
+					<TaskCard
+						task_id={taskList[0].id}
+						task_title={taskList[0].title}
+						task_progress={taskList[0].progress}
+						limit_time_org={taskList[0].limit_time}
+					/>
+				)}
+
+				{!topTask && (
+					<p className="text-lg text-red-600">タスクがありません。</p>
+				)}
 
 				<Footer />
 			</main>
