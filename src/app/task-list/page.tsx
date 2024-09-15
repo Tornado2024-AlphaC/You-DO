f985f487397342aa526869abec771b5ec3ce113f
KHashimoto3 <hashi.koki385@gmail.com>
2024-09-15 12:22:28 +0900

    feat: 納期一覧の実装を、limit-taskのpage.tsxに移動した。

--- src/app/task-list/page.tsx
+++ src/app/task-list/page.tsx
@@ -1,76 +1,33 @@
 'use client';
-import React, { useState } from 'react';
-import { CalendarToday } from '@mui/icons-material';
-import { format } from 'date-fns';
+
+import Footer from '@/components/features/footer/Footer';
+import { SideSwipe } from '@/components/ui/sideSwipe';
+import { TopTask } from '@/constants/routing';
 import { useRouter } from 'next/navigation';
-import TaskCard from '../../components/features/task-card/task-card';
+import React from 'react';
+import { useSwipeable } from 'react-swipeable';
+import { InTimer } from '../top-task/components/timer/Timer';
+import TaskField from './components/tasks/TaskField';
 
-// Tailwind CSS for styling
-const TaskList: React.FC = () => {
+const TaskList = () => {
 	const router = useRouter();
-	const [selectedDate, setSelectedDate] = useState(
-		format(new Date(), 'yyyy-MM-dd')
-	); // 今日の日付を初期値に設定
 
-	const handleViewSchedule = () => {
-		router.push('/daily-schedule');
-	};
-	// 日付選択ハンドラー
-	const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setSelectedDate(event.target.value);
-	};
-
+	const handlers = useSwipeable({
+		onSwipedRight: () => router.push(TopTask),
+		preventScrollOnSwipe: true,
+		trackMouse: true,
+	});
 	return (
-		<div className="min-h-screen flex flex-col justify-between items-center bg-white p-4">
-			{/* Header with Date */}
-			<div className="flex justify-between items-center w-full max-w-md mb-4">
-				<div className="flex items-center">
-					<input
-						type="date"
-						value={selectedDate}
-						onChange={handleDateChange}
-						className="bg-green-200 text-green-800 py-2 px-4 rounded-lg shadow-md"
-					/>
-				</div>
-			</div>
-			{/* Message */}
-			<div className="flex-grow flex justify-center items-center">
-				{/* ここをタスクがあるときない時に切り替える */}
-				{/*<p className="text-green-600 text-lg">本日納期のタスクはありません</p>*/}
-				<TaskCard id={1} taskName="タスクA" progress={10} />
-			</div>
-
-			{/* Footer Button */}
-			<div className="w-full flex justify-center">
-				<button
-					className="flex items-center bg-green-400 text-white py-2 px-4 rounded-lg shadow-md"
-					onClick={handleViewSchedule}
-				>
-					<span className="mr-2">← 1日の予定を見る</span>
-				</button>
-			</div>
-		</div>
+		<SideSwipe>
+			<main {...handlers}>
+				<TaskField taskTotal={7} />
+				{/* InTimer：空き時間中、NextTimer：空き時間外 */}
+				{/* <NextTimer /> */}
+				<InTimer />
+				<Footer />
+			</main>
+		</SideSwipe>
 	);
 };
 
 export default TaskList;
-
-// import { Profile, TopTask } from '@/constants/routing';
-// import Link from 'next/link';
-// import React from 'react';
-
-// const TaskList = () => {
-// 	return (
-// 		<main>
-// 			<h1>TaskList</h1>
-// 			<Link href={Profile} className="text-blue-500">
-// 				プロフィール画面へ
-// 			</Link>
-// 			<Link href={TopTask} className="text-blue-500">
-// 				今やるタスク画面へ
-// 			</Link>
-// 		</main>
-// 	);
-// };
-
-// export default TaskList;
