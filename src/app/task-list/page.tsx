31450f1aa9f6a743f0cac78a156f49b0ddc34a12
KHashimoto3 <hashi.koki385@gmail.com>
2024-09-15 11:38:41 +0900

    feat: タスク詳細表示で、タスクのidを扱えるようにした。

--- src/app/task-list/page.tsx
+++ src/app/task-list/page.tsx
@@ -7,61 +7,53 @@ import TaskCard from '../../components/features/task-card/task-card';
 
 // Tailwind CSS for styling
 const TaskList: React.FC = () => {
-  const router = useRouter();
-  const [selectedDate, setSelectedDate] = useState(format(new Date(), 'yyyy-MM-dd')); // 今日の日付を初期値に設定
+	const router = useRouter();
+	const [selectedDate, setSelectedDate] = useState(
+		format(new Date(), 'yyyy-MM-dd')
+	); // 今日の日付を初期値に設定
 
-  const handleViewSchedule = () => {
-    router.push('/daily-schedule'); 
-  };
-  // 日付選択ハンドラー
-  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedDate(event.target.value);
-  };
+	const handleViewSchedule = () => {
+		router.push('/daily-schedule');
+	};
+	// 日付選択ハンドラー
+	const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSelectedDate(event.target.value);
+	};
 
-  return (
-    <div className="min-h-screen flex flex-col justify-between items-center bg-white p-4">
-      {/* Header with Date */}
-      <div className="flex justify-between items-center w-full max-w-md mb-4">
-        <div className="flex items-center">
-          <input
-            type="date"
-            value={selectedDate}
-            onChange={handleDateChange}
-            className="bg-green-200 text-green-800 py-2 px-4 rounded-lg shadow-md"
-          />
-        </div>
-	  </div>
-      {/* Message */}
-      <div className="flex-grow flex justify-center items-center">
-	  	{/* ここをタスクがあるときない時に切り替える */}
-        <p className="text-green-600 text-lg">本日納期のタスクはありません</p> 
-		{/* <TaskCard/> */}
-      </div>
+	return (
+		<div className="min-h-screen flex flex-col justify-between items-center bg-white p-4">
+			{/* Header with Date */}
+			<div className="flex justify-between items-center w-full max-w-md mb-4">
+				<div className="flex items-center">
+					<input
+						type="date"
+						value={selectedDate}
+						onChange={handleDateChange}
+						className="bg-green-200 text-green-800 py-2 px-4 rounded-lg shadow-md"
+					/>
+				</div>
+			</div>
+			{/* Message */}
+			<div className="flex-grow flex justify-center items-center">
+				{/* ここをタスクがあるときない時に切り替える */}
+				{/*<p className="text-green-600 text-lg">本日納期のタスクはありません</p>*/}
+				<TaskCard id={1} taskName="タスクA" progress={10} />
+			</div>
 
-      {/* Footer Button */}
-      <div className="w-full flex justify-center">
-        <button
-          className="flex items-center bg-green-400 text-white py-2 px-4 rounded-lg shadow-md"
-          onClick={handleViewSchedule}
-        >
-          <span className="mr-2">← 1日の予定を見る</span>
-        </button>
-      </div>
-    </div>
-  );
+			{/* Footer Button */}
+			<div className="w-full flex justify-center">
+				<button
+					className="flex items-center bg-green-400 text-white py-2 px-4 rounded-lg shadow-md"
+					onClick={handleViewSchedule}
+				>
+					<span className="mr-2">← 1日の予定を見る</span>
+				</button>
+			</div>
+		</div>
+	);
 };
 
-
 export default TaskList;
-
-
-
-
-
-
-
-
-
 
 // import { Profile, TopTask } from '@/constants/routing';
 // import Link from 'next/link';
