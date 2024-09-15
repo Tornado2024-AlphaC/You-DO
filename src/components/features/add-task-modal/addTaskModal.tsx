6dc7e24e3f268124f010d8b1b31a0ea9e0e3664d
KHashimoto3 <hashi.koki385@gmail.com>
2024-09-15 23:32:17 +0900

    feat: 新しいタスクを追加できるようにした。

--- src/components/features/add-task-modal/addTaskModal.tsx
+++ src/components/features/add-task-modal/addTaskModal.tsx
@@ -8,6 +8,26 @@ import EventAvailableIcon from '@mui/icons-material/EventAvailable';
 import Image from 'next/image';
 import youDoAddTask from '../../../../public/add-task/Frame 100.svg';
 
+import { useUserData } from '../use-cookies/useUserData';
+
+type Task = {
+	id: number;
+	user_id: number;
+	title: string;
+	limit_time: string;
+	parent_id: number;
+	available_break: boolean;
+	duration: number;
+	expectation: number;
+	urgency: number;
+	firstexpect: number;
+	progress: number;
+	priority: number;
+	skip_count: number;
+	created_at: string;
+	updated_at: string;
+};
+
 // 型定義
 type TimeUnit = '10分' | '30分' | '1時間';
 
@@ -16,8 +36,14 @@ interface AddTaskModalProps {
 }
 
 const AddTaskModal: React.FC<AddTaskModalProps> = ({ closeModal }) => {
-	const [taskName, setTaskName] = useState('');
-	const [totalMinutes, setTotalMinutes] = useState(0);
+	const { getUserData } = useUserData();
+	const user_id = Number(getUserData().user_id);
+
+	//フォームの入力内容
+	const [task_name, set_task_name] = useState('');
+	const [total_minutes, set_total_minutes] = useState(0);
+	const [selected_date, set_selected_date] = useState('');
+	const [selected_time, set_selected_time] = useState('');
 
 	// 時間を分単位に変換して加算する関数
 	const addTime = (timeUnit: TimeUnit) => {
@@ -35,7 +61,7 @@ const AddTaskModal: React.FC<AddTaskModalProps> = ({ closeModal }) => {
 			default:
 				break;
 		}
-		setTotalMinutes(prev => prev + minutesToAdd);
+		set_total_minutes(prev => prev + minutesToAdd);
 	};
 
 	// 所要時間の分を時間と分の形式に変換する関数
@@ -47,7 +73,84 @@ const AddTaskModal: React.FC<AddTaskModalProps> = ({ closeModal }) => {
 
 	// 取消ボタンで時間をリセットする
 	const handleCancel = () => {
-		setTotalMinutes(0); // 初期値にリセット
+		set_total_minutes(0); // 初期値にリセット
+	};
+
+	const resetForm = () => {
+		set_task_name('');
+		set_total_minutes(0);
+		set_selected_date('');
+		set_selected_time('');
+	};
+
+	const save_new_task = async (
+		user_id: number,
+		title: string,
+		limit_time: string,
+		expectation: number
+	): Promise<Task> => {
+		return new Promise((resolve, reject) => {
+			const url = 'api/task';
+			const obj = {
+				user_id,
+				title,
+				limit_time,
+				parent_id: 0,
+				available_break: true,
+				expectation,
+			};
+
+			try {
+				fetch(url, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(obj),
+				}).then(async res => {
+					if (!res.ok) {
+						const statusCode = res.status;
+						switch (statusCode) {
+							case 400:
+								throw new Error('Bad Request');
+							case 500:
+								throw new Error('Internal Server Error');
+							default:
+								throw new Error('Unknown Error');
+						}
+					}
+					const data = await res.json(); //okだった時の処理
+					resolve(data.task);
+				});
+			} catch (error) {
+				alert('D: apiで登録中にエラーが発生しました。');
+				reject(null);
+			}
+		});
+	};
+
+	const save_task = () => {
+		const title = task_name;
+		const selected_date_time = selected_date + 'T' + selected_time + ':00.000';
+		const total_expectation = total_minutes * 60 * 1000;
+
+		try {
+			const taskData = save_new_task(
+				user_id,
+				title,
+				selected_date_time,
+				total_expectation
+			);
+			if (!taskData) {
+				alert('A: タスクが登録できませんでした。');
+				return;
+			}
+			alert('B: タスクが登録されました。');
+			resetForm();
+		} catch (error) {
+			console.log(error);
+			alert('C: タスクが登録できませんでした。');
+		}
 	};
 
 	return (
@@ -64,10 +167,10 @@ const AddTaskModal: React.FC<AddTaskModalProps> = ({ closeModal }) => {
 					<input
 						type="text"
 						id="taskName"
-						value={taskName}
+						value={task_name}
 						placeholder="タスク名を追加"
 						className="outline-none text-lg w-full border-b border-gray-300 pb-2"
-						onChange={e => setTaskName(e.target.value)}
+						onChange={e => set_task_name(e.target.value)}
 					/>
 				</div>
 				<hr />
@@ -92,8 +195,16 @@ const AddTaskModal: React.FC<AddTaskModalProps> = ({ closeModal }) => {
 					<p className="text-lg">納期を設定</p>
 				</div>
 				<div className="flex flex-row gap-3">
-					<input type="date" />
-					<input type="time" />
+					<input
+						type="date"
+						value={selected_date}
+						onChange={e => set_selected_date(e.target.value)}
+					/>
+					<input
+						type="time"
+						value={selected_time}
+						onChange={e => set_selected_time(e.target.value)}
+					/>
 				</div>
 				<hr />
 			</div>
@@ -107,7 +218,7 @@ const AddTaskModal: React.FC<AddTaskModalProps> = ({ closeModal }) => {
 					/>
 					<h3 className="text-gray-700 font-medium">所要時間</h3>
 					<span className="text-green-600 ml-auto">
-						{formatTime(totalMinutes)}
+						{formatTime(total_minutes)}
 					</span>
 				</div>
 
@@ -145,7 +256,10 @@ const AddTaskModal: React.FC<AddTaskModalProps> = ({ closeModal }) => {
 					>
 						← 戻る
 					</button>
-					<button className="flex items-center justify-center px-6 py-2 bg-green-400 text-white rounded-lg">
+					<button
+						className="flex items-center justify-center px-6 py-2 bg-green-400 text-white rounded-lg"
+						onClick={save_task}
+					>
 						＋ タスク追加
 					</button>
 				</div>
